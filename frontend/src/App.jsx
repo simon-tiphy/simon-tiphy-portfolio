6a5e11d18e9394b1a09c01820b0b5d8f9cd52ad3
simon tiphy <simontiphy@gmail.com>
2025-11-27 02:22:55 +0300

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline m-9 text-center">
+      Hello, Vite + React + TailwindCSS!
+      
+    </div>
+  )
+}
+
+export default App
